ab93708daede6db5d65bd9ea79c56b69699af4c9
iamnpradeep <pnakire@ufl.edu>
2014-02-28 17:42:39 -0500

    Changes to print method

--- src/zappos1/ZapposChallenge.java
+++ src/zappos1/ZapposChallenge.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.*;
-
 import javax.net.ssl.HttpsURLConnection;
 
 class Products {
@@ -26,29 +25,38 @@ public class ZapposChallenge {
  
 	private final String USER_AGENT = "Mozilla/5.0";
 	
-	public void printitems(ArrayList<String> pName,ArrayList<Float> pPrice, int n) {
+	public void printitems(ArrayList<String> pName, ArrayList<Float> pPrice, ArrayList<String> pUrl, int n) {
 		//int iterator = pPrice.size()/n;
-		System.out.println(pName);
-		System.out.println(pPrice);
+		//System.out.println(pName);
+		//System.out.println(pPrice);
 		Random r = new Random();
 		int  i=0;
 		int randomIndex=0;
-		System.out.println(pName.size());
+		//System.out.println(pName.size());
 		int size = pName.size();
 		int rounds = (2*size)/3;
-		System.out.println(rounds);
+		//int rounds = size;
+		//System.out.println(rounds);
+		System.out.println(rounds+" sets of suggestions according to search");
+		System.out.println();
 		for(int j =0;j<rounds;j++){
-		for (i = 0; i < n; i++) {
-			randomIndex = r.nextInt(pName.size());
-			System.out.println(pName.get(randomIndex)+"   "+pPrice.get(randomIndex));
-		}
+			for (i = 0; i < n; i++) {
+				randomIndex = r.nextInt(pName.size());
+				System.out.println("Product Name: "+pName.get(randomIndex)+"   Price: "+pPrice.get(randomIndex));
+				System.out.println("Product URL: "+pUrl.get(randomIndex));
+			}
 		
-		System.out.println();
-		System.out.println();
-		System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
 		
-	}
+	   }
+		
+			if(rounds==0){
 			
+							System.out.println("Sorry! No match of items according to search");
+							System.out.println("Please try again with different values");
+						 }
 	
 	}
  
@@ -97,7 +105,7 @@ public class ZapposChallenge {
 		cost = Float.parseFloat(myInput1);
 		br.close();
 		
-		if(no_of_items!=0)
+		if(no_of_items!=0 && cost!=0)
 		{		
 		Individual_Cost= cost/no_of_items;
 		
@@ -108,7 +116,9 @@ public class ZapposChallenge {
 		
 		ArrayList<String> productName = new ArrayList<String>();
 		ArrayList<Float> Price = new ArrayList<Float>();
+		ArrayList<String> ProductUrl = new ArrayList<String>();
 		ArrayList<String> urls = new ArrayList<String>();
+		
 		urls.add("http://api.zappos.com/Search?term=boots&key=a73121520492f88dc3d33daf2103d7574f1a3166");
 		urls.add("http://api.zappos.com/Search?term=beauty&key=a73121520492f88dc3d33daf2103d7574f1a3166");
 		urls.add("http://api.zappos.com/Search?term=clothing&key=a73121520492f88dc3d33daf2103d7574f1a3166");
@@ -116,19 +126,20 @@ public class ZapposChallenge {
 		urls.add("http://api.zappos.com/Search?term=jewelry&key=a73121520492f88dc3d33daf2103d7574f1a3166");
 		urls.add("http://api.zappos.com/Search?term=men&key=a73121520492f88dc3d33daf2103d7574f1a3166");
 		urls.add("http://api.zappos.com/Search?term=eyewear&key=a73121520492f88dc3d33daf2103d7574f1a3166");
-		urls.add("http://api.zappos.com/Search?term=women&key=a73121520492f88dc3d33daf2103d7574f1a3166");
-		urls.add("http://api.zappos.com/Search?term=kids&key=a73121520492f88dc3d33daf2103d7574f1a3166");
+		//urls.add("http://api.zappos.com/Search?term=women&key=a73121520492f88dc3d33daf2103d7574f1a3166");
+		//urls.add("http://api.zappos.com/Search?term=kids&key=a73121520492f88dc3d33daf2103d7574f1a3166");
 		//urls.add("http://api.zappos.com/Search?term=accessories&key=a73121520492f88dc3d33daf2103d7574f1a3166");
 		//urls.add("http://api.zappos.com/Search?term=new%20arrivals&key=a73121520492f88dc3d33daf2103d7574f1a3166");
 
 		
 		System.out.println("Testing 1 - Send Http GET request");
 		for(int j=0;j<urls.size();j++)
-		http.sendGet(productName,Price,urls.get(j),Individual_Cost);
+		http.sendGet(productName,Price,ProductUrl,urls.get(j),Individual_Cost);
  
-		System.out.println(productName);
-		System.out.println(Price);
-		http.printitems(productName, Price, no_of_items);
+		//System.out.println(productName);
+		//System.out.println(Price);
+		//System.out.println(ProductUrl);
+		http.printitems(productName, Price, ProductUrl, no_of_items);
  
 		}
 		else{
@@ -138,7 +149,7 @@ public class ZapposChallenge {
 		
  
 	// HTTP GET request
-	private void sendGet(ArrayList<String> pName, ArrayList<Float> pPrice, String url,float cost) throws Exception {
+	private void sendGet(ArrayList<String> pName, ArrayList<Float> pPrice,ArrayList<String> pUrl, String url,float cost) throws Exception {
  
 		
 		//String url = "http://api.zappos.com/Search?term=boots&key=a73121520492f88dc3d33daf2103d7574f1a3166d";
@@ -180,6 +191,7 @@ public class ZapposChallenge {
 		String prName;
 		float Price=0;
 		String temp;
+		String producturl;
 		for(i=0; i<n.length(); i++){
 //		        p[i].price = n.getJSONObject(i).getString("price");
 		        //p[i].name = n.getJSONObject(i).getString("productName");
@@ -187,11 +199,13 @@ public class ZapposChallenge {
 				temp = temp.substring(1);
 				Price = Float.parseFloat(temp);
 				prName = n.getJSONObject(i).getString("productName");
+				producturl= n.getJSONObject(i).getString("productUrl");
 				
 				if (Price > cost-10 && Price<=cost) {
 				
 				pPrice.add(Price);
 		        pName.add(prName);
+		        pUrl.add(producturl);
 				}
 		    }
 
